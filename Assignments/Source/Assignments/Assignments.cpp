39e2e2b3200619c0512ca282042624d61dc71389
Sinil Kang <rtd99062@gmail.com>
2023-10-04 12:16:05 +0900

    #0 (Engine Setup) - Created a project for indirect textures.
    Engine essential assets

---
+++ Assignments/Source/Assignments/Assignments.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Assignments.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Assignments, "Assignments" );
---
+++ Assignments/Source/Assignments/AssignmentsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AssignmentsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASSIGNMENTS_API AAssignmentsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
